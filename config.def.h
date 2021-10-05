819456757c3541d43fd155e4dd52b72469c2ed92
Samuel <bdzsamuel@gmail.com>
2021-10-05 21:00:36 +0200

    Update font family and font size

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode-Medium:pixelsize=12:antialias=true:autohint=true";
+static char *font = "CascadiaCode-Regular:pixelsize=13:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
